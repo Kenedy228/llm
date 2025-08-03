2a8f9e1fdc412b521595e3911210a35cb63c539d
Данил Журавлев <zhuravleffdanilka2004@mail.ru>
2025-08-03 22:48:57 +1000

    add basic structure

--- llm-project/main.py
+++ llm-project/main.py
@@ -1,5 +1,56 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+model = "gemini-2.0-flash-001"
+
+
+def handle_user_prompt(args):
+    if len(args) == 1:
+        print("exec failure: provide a valid prompt \"uv run main.py <prompt> <args>\"")
+        sys.exit(1)
+
+    user_prompt, verbose = None, False
+
+    if len(args) == 2:
+        if args[1] == "--verbose":
+            print("exec failure: provide a valid prompt \"uv run main.py <prompt> <args>\"")
+            sys.exit(1)
+        user_prompt = args[1]
+
+    if len(args) == 3:
+        if args[1] == "--verbose":
+            print("exec failure: provide a valid prompt \"uv run main.py <prompt> <args>\"")
+            sys.exit(1)
+        if args[2] != "--verbose":
+            print("exec failure: unknown flag")
+            sys.exit(1)
+        user_prompt = args[1]
+        verbose = args[2]
+
+    return user_prompt, verbose
+
+
+def generate_report(response, user_prompt, verbose=False):
+    print(response.text)
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
 def main():
-    print("Hello from llm-project!")
+    user_prompt, verbose = handle_user_prompt(sys.argv)
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    ]
+    response = client.models.generate_content(model=model, contents=messages)
+    generate_report(response, user_prompt, verbose)
 
 
 if __name__ == "__main__":
